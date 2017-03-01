918ad5ce91ab063120159f17f661063d3713556e
Goran Erhartič <Goran Erharti>
2017-03-01 20:53:04 +0100

    Removed custom autoloader. Edited composer.json so it implements PSR-4 type namespace autoloading of custom classes

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -5,11 +5,6 @@
  * Date: 23/2/2017
  * Time: 8:38 AM
  */
-// Define a path where Classes will be stored
-define("CLASS_PATH", dirname(__DIR__) . "\\src");
-
-// Include the core autoloader.
-require_once __DIR__ . '/../src/autoloader.php';
 
 //Include composer autoload.
 require_once __DIR__ . '/../vendor/autoload.php';
